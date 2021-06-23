7cb55c99c40f5d939898e95dd03ef6991ea7c355
Agustin Olano <agustinolanoit@gmail.com>
2021-06-22 21:11:05 -0300

    rehago la pagina desde una perspectiva mobile first, modifico estilos. Todavia falta hacer el footer, ver el envio de mails y la vista desde una PC

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,24 +1,24 @@
-document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", () => {
     const slider = document.querySelector("#slider");
     let sliderSection = document.querySelectorAll(".slider-section");
     let lastImage = sliderSection[sliderSection.length - 1];
 
-    slider.insertAdjacentElement("afterbegin",lastImage);
+    slider.insertAdjacentElement("afterbegin", lastImage);
 
-    function Next(){
+    function Next() {
         let sliderSection = document.querySelectorAll(".slider-section");
         let firstImage = sliderSection[0];
 
         slider.style.marginLeft = "-200%";
         slider.style.transition = "all 0.5s";
-        setTimeout(function(){
+        setTimeout(function () {
             slider.style.transition = "none";
-            slider.insertAdjacentElement("beforeend",firstImage);
+            slider.insertAdjacentElement("beforeend", firstImage);
             slider.style.marginLeft = "-100%";
-        },500);
+        }, 500);
     }
 
     setInterval(() => {
         Next();
     }, 2800);
-})
+});
